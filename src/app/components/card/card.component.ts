3b1f607c2a69f3ffa0740901ce3de94e347fd0ea
Rubendvb <rubenvasquez556@gmail.com>
2024-01-16 09:05:19 -0300

    first commit

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { PokemonData } from '../../models/pokemonData';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+})
+export class CardComponent implements OnInit {
+  pokemon: PokemonData;
+
+  constructor(private service: PokemonService) {
+    this.pokemon = {
+      id: 0,
+      name: 'pikachu',
+      sprites: {
+        other: {
+          dream_world: {
+            front_default: '',
+          },
+        },
+      },
+      types: [],
+    };
+  }
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu');
+  }
+
+  getPokemon(searchName: string) {
+    this.service.getPokemon(searchName).subscribe({
+      next: (res) => {
+        this.pokemon = {
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types,
+        };
+      },
+      error: (err) => console.log(err),
+    });
+  }
+}
